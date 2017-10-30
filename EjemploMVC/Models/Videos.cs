927a8612673d44584ccaef86c0b404aae2b39a9b
Felix <felix.avendano@tecandweb.com>
2017-10-30 18:04:11 +0100

    grabar

---
+++ EjemploMVC/Models/Videos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EjemploMVC.Models
+{
+    public class Videos
+    {
+        public int ID { get; set; }
+        public string Titulo { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime FechaEstreno { get; set; }
+        public string Genero { get; set; }
+        public decimal Precio { get; set; }
+
+    }
+}
